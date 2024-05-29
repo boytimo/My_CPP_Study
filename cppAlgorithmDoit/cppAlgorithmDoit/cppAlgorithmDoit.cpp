4f6f1e6a2be9e464e4140a460e30c63b76f8d45d
boytimo <boytimo@naver.com>
2024-05-29 20:03:55 +0900

    나머지 합 구하기 해결X

--- cppAlgorithmDoit/cppAlgorithmDoit/cppAlgorithmDoit.cpp
+++ cppAlgorithmDoit/cppAlgorithmDoit/cppAlgorithmDoit.cpp
@@ -69,6 +69,7 @@ using namespace std;
 //}
 
 //--------------------------------------------------------
+// //구간 합 1
 //int main()
 //{
 //	cout << "데이터의 개수[배열개수], 질의 개수를 입력하세요";
@@ -123,45 +124,106 @@ using namespace std;
 //		cout << "결과: " << result << endl;
 //	}
 //}
+//------------------------------------------------
+//구간 합 2
+//int main()
+//{
+//	cout << "2차원 배열 크기와 구간 합의 질의 갯수를 입력하세요. ";
+//	int N, M;
+//	cin >> N >> M;
+//	
+//	vector<vector<int>>S(N+1,vector<int>(N+1,0));
+//	//vector<int>S(10,0); -> 10개의 배열에 0을 모두 대입
+//
+//	for (int i = 1; i <= N; i++)
+//	{
+//		cout << "배열 안의 값을 입력해주세요. ";
+//		int data;
+//		for (int j = 1; j <= N; j++)
+//		{
+//			
+//			cin >> data;
+//			S[i][j] = data + S[i][j-1];
+//
+//		}
+//	}
+//
+//	
+//	for (int i = 0; i < M; i++)
+//	{
+//		int result = 0;
+//
+//		int x1 =0, y1=0, x2=0, y2=0;
+//		cout << "(X1, Y1) (X2, Y2)를 입력 해주세요";
+//		cin >> x1 >> y1 >> x2 >> y2;
+//
+//		for (int j = x1; j <= x2; j++)
+//		{
+//			result += S[j][y2] - S[j][y1 - 1];
+//		}
+//
+//		cout << "결과 값: " << result << endl;
+//	}
+//
+//	
+//}
+
+//-----------------------------------------------
 
 int main()
 {
-	cout << "2차원 배열 크기와 구간 합의 질의 갯수를 입력하세요. ";
-	int N, M;
-	cin >> N >> M;
-	
-	vector<vector<int>>S(N+1,vector<int>(N+1,0));
-	//vector<int>S(10,0); -> 10개의 배열에 0을 모두 대입
+	int n,m;
+	cout << "원소 개수와 나눌 수 M을 입력하세요. ";
+	cin >>n>> m;
 
-	for (int i = 1; i <= N; i++)
+	vector<int>D(n+1, 0); //원본 데이터
+	vector<int>S(n+1, 0); //합 데이터
+	vector<int>S2(n + 1, 0); //합데이터를 m으로 나눈 변경된 합 데이터
+	vector<int>r(m - 1, 0); //나머지 값 개수 별 정리 데이터
+
+	int result = 0; //결과값
+
+	cout << "원소를 입력하세요. ";
+	for (int i = 1; i <= n; i++)
 	{
-		cout << "배열 안의 값을 입력해주세요. ";
-		int data;
-		for (int j = 1; j <= N; j++)
-		{
-			
-			cin >> data;
-			S[i][j] = data + S[i][j-1];
+		cin >> D[i];
+		S[i] = S[i - 1] + D[i];
 
+		if (!(S2[i] = S[i] % m)) //M에 m을 나눈 데이터를 저장
+		{
+			result++;
+		}
+
+		r[S2[i]] += 1; 
+	}
+
+	if (S2[0] == 0)
+	{
+		r[0] -= 1;
+	}
+
+	for (int i = 0; i < m - 1; i++)
+	{
+		if (r[i] > 1)
+		{
+			int facto = 0, facto2 = 0;
+			for (int j = r[i]; j > 0; j--)
+			{
+				facto *= j;
+			}
+			for (int j = r[i] - 2; j > 0; j--)
+			{
+				facto2 *= j;
+			}
+
+			result += (facto) / (facto2 * r[i]);
 		}
 	}
 
-	
-	for (int i = 0; i < M; i++)
-	{
-		int result = 0;
+	//m으로 나눈 나머지가 x일때 = 0 ~ m-1
+	//nCr = n! / (n-r)! *r
 
-		int x1 =0, y1=0, x2=0, y2=0;
-		cout << "(X1, Y1) (X2, Y2)를 입력 해주세요";
-		cin >> x1 >> y1 >> x2 >> y2;
+	cout << "결과 값: " << result << endl;
 
-		for (int j = x1; j <= x2; j++)
-		{
-			result += S[j][y2] - S[j][y1 - 1];
-		}
 
-		cout << "결과 값: " << result << endl;
-	}
-
-	
 }
